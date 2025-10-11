d7aeaa7d094c1ac8ff2f6e26ada1d2c1917b5ef1
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-10-11 08:14:43 -0700

    Update format to remove comma between datetime

--- src/core/transforms/format.js
+++ src/core/transforms/format.js
@@ -111,14 +111,9 @@ function formatLocalizedTime(date) {
  */
 function formatLocalizedDateTime(date) {
   try {
-    return new Intl.DateTimeFormat(undefined, {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    }).format(date);
+    const dateStr = formatLocalizedDate(date);
+    const timeStr = formatLocalizedTime(date);
+    return `${dateStr} ${timeStr}`;
   } catch (error) {
     return date.toISOString().replace('T', ' ').slice(0, 19);
   }
